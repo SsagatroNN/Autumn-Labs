ef57da5150f786127c7e53b137948a452c2de608
SsagatroNN <omar.khatib03@outlook.com>
2022-10-28 20:13:56 +0300

    Added sinegen files

---
+++ task1/sinegen_tb.cpp
@@ -0,0 +1,64 @@
+//mandatory header files used for all c++ files
+
+#include "Vsinegen.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "vbuddy.cpp"
+
+int main(int argc, char **argv, char **env){
+    //countes number of clock cycles
+
+    int i;
+    int clk;
+
+    //intialize vconter
+
+    Vsinegen * sinegenone = new Vsinegen;
+
+    //turn on signal tracing
+
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    sinegenone->trace (tfp,99);
+    tfp->open ("counter.vcd");
+
+    //init vbuddy 
+    if (vbdOpen() != 1) return (-1);
+    vbdHeader("Lab 1: Counter");
+
+    //intitialize simulation inputs
+
+    sinegenone->clk = 1;
+    sinegenone->rst = 0;
+    sinegenone->en = 1;
+
+    //loop to run for many clock cycles:
+
+    for(i = 0; i < 300; i++){
+
+        for (clk = 0; clk < 2; clk++){
+            tfp->dump (2 * i + clk);
+            sinegenone->clk = !sinegenone->clk;
+            sinegenone->eval ();
+        }
+
+
+
+        //+++++ send count value to vbuddy
+        vbdPlot();
+        vbdCycle( i + 1);
+        //----- end of vbuddy output section
+
+        //adjust rst and en signals
+        //with counting ssinegenoneping at a count of 9
+
+        sinegenone->rst = (i<2) | (i==19);
+        sinegenone->en = ((14>i) && (i>=5)) || i > 15 ;
+        if (Verilated::gotFinish()) exit(0);
+
+    }
+
+    vbdClose();    //+++++++
+    tfp->close();
+    exit(0);
+}
